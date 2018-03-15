15dd9fc36ed8ee6d6a6550d60fd70483199e66e0
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-15 22:23:04 +0000

    Create searchDeposit.js

---
+++ js/searchDeposit.js
@@ -0,0 +1,22 @@
+  $("#imaginary_container > div:nth-child(1) > input").keyup(function() {
+    var $ps = $("#search-dropdown > p");
+    var val = $.trim(this.value).toUpperCase();
+    if (val === "") {
+        $("#search-dropdown").hide(); 
+    }
+    else {
+        $("#search-dropdown").show(); 
+        $ps.hide();
+        $ps.filter(function() {
+            return -1 != $(this).text().toUpperCase().indexOf(val); }).show();
+    } 
+    $ps.one("click", function() {
+      if(localStore.getItem("depositToken") !== $(this).attr("fullname")) {
+        localStore.setItem("depositToken",$(this).attr("fullname"));       
+      }
+      $('.depositToken').text(localStore.getItem("depositToken"));
+      $("#imaginary_container > div:nth-child(1) > input").val('');      
+      $("#search-dropdown").hide();  
+    });
+  });
+
