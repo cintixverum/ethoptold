617b1edf9b5bf645a9e5e3d60f3c53c13bc1a4c3
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-15 19:47:46 +0000

    Update searchResult.js

--- js/searchResult.js
+++ js/searchResult.js
@@ -1,6 +1,6 @@
 $.get("https://www.optionsdexapi.tk/getApprovedTokens",function(tokenArray,status) {
     tokenArray.forEach(function(arr) {
-        let paragraph = '<p fullResult="' + arr['token_name'] + '" decimals="' + arr['decimals'] +  '" address="' + arr['token_address'] + '" >' + arr['token_symbol'] + '</p>'
+        let paragraph = '<p fullName="' + arr['token_name'] + '" decimals="' + arr['decimals'] +  '" address="' + arr['token_address'] + '" >' + arr['token_symbol'] + '</p>'
         console.log(paragraph);
         $('#search-dropdown').append(paragraph);
     });
