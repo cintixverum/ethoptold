0a22ad68ed9c6d03d802396f0bdc97d63551ae8e
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-15 13:56:47 +0000

    Add files via upload

---
+++ js/metamask.js
@@ -0,0 +1,18 @@
+(function(){
+  setInterval(function() {
+    web3.eth.getAccounts(function (err, accounts) {
+      if(err) return;
+      let element = document.getElementById("metamask");
+      if(accounts[0]) {
+        element.style.color = "green";
+        element.textContent = "metamask unlocked";
+        localStorage.setItem("userAddress",accounts[0]);
+      }
+      else {
+        element.style.color = "red";
+        element.textContent = "metamask locked";
+      }  
+    })
+  }, 5000)
+})();
+
