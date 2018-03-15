5a39b0561c5f576ad011c1dbbd4d955b445a099e
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-15 18:48:06 +0000

    Update searchFilter.js

--- js/searchFilter.js
+++ js/searchFilter.js
@@ -1,6 +1,7 @@
   var $ps = $("#search-dropdown > p");
   $("#imaginary_container > div:nth-child(1) > input").keyup(function() {
     var val = $.trim(this.value).toUpperCase();
+    console.log(val);
     if (val === "") {
         $("#search-dropdown").hide(); 
     }
@@ -8,7 +9,6 @@
         $("#search-dropdown").show(); 
         $ps.show();
         $ps.filter(function() {
-            console.log($(this).text(),val);
             return -1 !== $(this).text().toUpperCase().indexOf(val); }).show();
     }
   });
