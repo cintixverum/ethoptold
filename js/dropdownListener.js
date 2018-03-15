a2632b8e55d481d2e5a80c62fc753c900db66254
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-15 22:11:16 +0000

    Update dropdownListener.js

--- js/dropdownListener.js
+++ js/dropdownListener.js
@@ -1,7 +1,9 @@
 $('#baseToken').on('change', function() {
   localStore.setItem("baseToken",this.value);
+  $('.baseToken').text(localStore.getItem("baseToken"));
 });
 
 $('#timeframe').on('change', function() {
   localStore.setItem("timeframe",this.value);
+  $('.timeframe').text(localStore.getItem("timeframe")); 
 })
