a340abb3e26bf5c05c9df1ee9b0abf6c6c3ea6cb
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-15 16:22:24 +0000

    Update dropdownListener.js

--- js/dropdownListener.js
+++ js/dropdownListener.js
@@ -2,10 +2,8 @@ const localStore = storageFactory;
       
 $('#baseToken').on('change', function() {
   localStore.setItem("baseToken",this.value);
-  console.log(localStore.getItem("baseToken"));
 });
 
 $('#timeframe').on('change', function() {
   localStore.setItem("timeframe",this.value);
-  console.log(localStore.getItem("timeframe"));
 })
