cbcf5cc031c082583c96ddcf620636367da56272
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-15 18:34:32 +0000

    Create searchFilter.js

---
+++ js/searchFilter.js
@@ -0,0 +1,14 @@
+  var $ps = $("#search-dropdown > p");
+  $("#imaginary_container > div:nth-child(1) > input").keyup(function() {
+    var val = $.trim(this.value).toUpperCase();
+    console.log(val);
+    if (val === "") {
+        $("#search-dropdown").hide(); 
+    }
+    else {
+        $("#search-dropdown").show(); 
+        $ps.hide();
+        $ps.filter(function() {
+            return -1 != $(this).text().toUpperCase().indexOf(val); }).show();
+    }
+  });
