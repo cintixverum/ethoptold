534aa597de56085d1f8767e815bbb9b8f20d0ecf
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-16 15:25:58 +0000

    Delete populatePage.js

--- js/populatePage.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $('.baseToken').text(localStore.getItem("baseToken"));
-  $('.tradedToken').text(localStore.getItem("tradedToken"));
-  $('.timeframe').text(localStore.getItem("timeframe"));
-  updateOrderbook();
-});  
