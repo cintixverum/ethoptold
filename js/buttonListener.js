de4a1e64990ec1380be397b3334b9aac5a4b97b9
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-16 12:43:04 +0000

    Update buttonListener.js

--- js/buttonListener.js
+++ js/buttonListener.js
@@ -1,15 +1,19 @@
 $('#buySell > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
   localStore.setItem("buySell",$(this).find('input').val());
+  location.reload();  
 });
 
 $('#buySell > label.btn.btn-default.btn-off.btn-sm').on('click', function() {
   localStore.setItem("buySell",$(this).find('input').val());  
+  location.reload();  
 });
 
 $('#optionType > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
   localStore.setItem("optionType",$(this).find('input').val()); 
+  location.reload();  
 });
 
 $('#optionType > label.btn.btn-default.btn-off.btn-sm').on('click', function() {
   localStore.setItem("optionType",$(this).find('input').val());  
+  location.reload();  
 });
