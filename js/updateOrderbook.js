7a7928f52fdb5a0f5607cf626dcf9027262e13e8
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-16 16:54:05 +0000

    Update updateOrderbook.js

--- js/updateOrderbook.js
+++ js/updateOrderbook.js
@@ -1,7 +1,7 @@
 function updateOrderbook() {
   let tradedTokenQuery = "p[fullname='" + localStore.getItem("tradedToken") + "']";
   let baseTokenQuery = "p[fullname='" + localStore.getItem("baseToken") + "']";
-  let timeframeQuery = "option[data='" + localStore.getItem("timeseconds") + "']";
+  let timeframeQuery = "option[date='" + localStore.getItem("timeseconds") + "']";
   console.log(tradedTokenQuery,baseTokenQuery,timeframeQuery);
   let tradedTokenAddress = $(tradedTokenQuery).attr("address");
   let baseTokenAddress = $(baseTokenQuery).attr("address");
