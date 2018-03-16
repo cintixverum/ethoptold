95a979f71321ed941e19c18915bb737387b365a9
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-16 10:40:23 +0000

    Update buttonListener.js

--- js/buttonListener.js
+++ js/buttonListener.js
@@ -4,6 +4,7 @@ $('#buySell > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
   let baseTokenQuery = "p[fullname='" + localStore.getItem("baseToken") + "']";  
   let tradedTokenAddress = $(tradedTokenQuery).attr("address");
   let baseTokenAddress = $(baseTokenQuery).attr("address");
+  console.log(tradedTokenAddress,baseTokenAddress);
 });
 
 $('#buySell > label.btn.btn-default.btn-off.btn-sm').on('click', function() {
