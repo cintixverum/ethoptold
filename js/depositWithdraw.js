f3d05772ff818fae626b4749e1800424ef850572
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-17 11:45:52 +0000

    Update depositWithdraw.js

--- js/depositWithdraw.js
+++ js/depositWithdraw.js
@@ -1,7 +1,7 @@
 $('#status > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
-  $('#body > div.wallet-container > div > div > form > input').value('Deposit');
+  $('#body > div.wallet-container > div > div > form > input').val('Deposit');
 });
 
 $('#status > label.btn.btn-default.btn-off.btn-sm').on('click', function() {
-  $('#body > div.wallet-container > div > div > form > input').value('Withdraw');
+  $('#body > div.wallet-container > div > div > form > input').val('Withdraw');
 });
