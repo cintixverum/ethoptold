0a8f2192c0e120e5cf494731e2cfbf8965e82aed
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-17 13:39:55 +0000

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,6 +1,6 @@
 $("input[value='Deposit']").on('click', function() {
   let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
-  let depositTokenAddress = $(tradedTokenQuery).attr("address");
+  let depositTokenAddress = $(depositTokenQuery).attr("address");
   let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());
   console.log("wallet: ",depositTokenAddress," value: ",value);  
   depositTokenAddress === "0x0000000000000000000000000000000000000000" ? sendTransaction(walletFunctions.depositETH(value)) : sendTransaction(walletFunctions.depositToken(depositTokenAddress,value)); 
@@ -8,7 +8,7 @@ $("input[value='Deposit']").on('click', function() {
 
 $("input[value='Withdraw']").on('click', function() {
   let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
-  let depositTokenAddress = $(tradedTokenQuery).attr("address");
+  let depositTokenAddress = $(depositTokenQuery).attr("address");
   let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());  
   console.log("wallet: ",depositTokenAddress," value: ",value);  
   depositTokenAddress === "0x0000000000000000000000000000000000000000" ? sendTransaction(walletFunctions.withdrawETH(value)) : sendTransaction(walletFunctions.withdrawToken(depositTokenAddress,value));   
