25ea9cfdfce529e46c52ac59ec7a11c5b3cf67ef
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-17 13:17:08 +0000

    Update searchDeposit.js

--- js/searchDeposit.js
+++ js/searchDeposit.js
@@ -11,7 +11,7 @@ $("#imaginary_container > div:nth-child(1) > input").keyup(function() {
             return -1 != $(this).text().toUpperCase().indexOf(val); }).show();
     } 
     $ps.one("click", function() {
-      if(localStore.getItem("depositToken") !== $(this).attr("fullname")) {
+      if($('.depositToken').text() !== $(this).attr("fullname")) {
         localStore.setItem("depositToken",$(this).attr("fullname"));  
         $('.depositToken').text(localStore.getItem("depositToken"));
         $("#imaginary_container > div:nth-child(1) > input").val('');      
