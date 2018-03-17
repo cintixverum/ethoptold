595d3019c731d8b32bb66fad65ea5637e4bd73e3
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-17 16:24:16 +0000

    Update sendTransaction.js

--- js/sendTransaction.js
+++ js/sendTransaction.js
@@ -2,6 +2,7 @@ function sendTransaction(Tx,callback) {
   web3.eth.sendTransaction(Tx, function(err,transactionHash) {
     if (err) return;
     console.log("Transaction Hash: ", transactionHash," Err: ",err);
+    console.log(callback);
     if(callback) {
       callback();
     }  
