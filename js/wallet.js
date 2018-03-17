f9fe1c1a9082937d40b220fc38b919491bb353fe
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-17 14:27:34 +0000

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,9 +1,16 @@
+function depositToken() {
+  let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
+  let depositTokenAddress = $(depositTokenQuery).attr("address");
+  let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());
+  walletFunctions.depositToken(depositTokenAddress,value);
+}  
+
 $("input[value='Deposit']").on('click', function() {
   let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
   let depositTokenAddress = $(depositTokenQuery).attr("address");
   let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());
   console.log("wallet: ",depositTokenAddress," value: ",value);  
-  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? sendTransaction(walletFunctions.depositETH(value)) : sendTransaction(tokenFunctions.approveToken(value),walletFunctions.depositToken(depositTokenAddress,value)); 
+  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.depositETH(value) : tokenFunctions.approveToken(value,depositToken); 
 });
 
 $("input[value='Withdraw']").on('click', function() {
@@ -11,5 +18,5 @@ $("input[value='Withdraw']").on('click', function() {
   let depositTokenAddress = $(depositTokenQuery).attr("address");
   let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());  
   console.log("wallet: ",depositTokenAddress," value: ",value);  
-  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? sendTransaction(walletFunctions.withdrawETH(value)) : sendTransaction(walletFunctions.withdrawToken(depositTokenAddress,value));   
+  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.withdrawETH(value) : walletFunctions.withdrawToken(depositTokenAddress,value);   
 });
