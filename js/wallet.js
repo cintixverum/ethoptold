a2504fd280176c27ec0ee89d39fa572aa863584b
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-17 11:59:02 +0000

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,21 +1,11 @@
 $('input[value="Deposit"]').one('click', function() {
   let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
   let depositTokenAddress = $(tradedTokenQuery).attr("address");
-  if(depositTokenAddress === "0x0000000000000000000000000000000000000000") {
-    sendTransaction(walletFunctions.depositETH());
-  }
-  else { 
-    sendTransaction(walletFunctions.depositToken(depositTokenAddress));
-  }   
+  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? sendTransaction(walletFunctions.depositETH()) : sendTransaction(walletFunctions.depositToken(depositTokenAddress)); 
 });
 
 $('input[value="Withdraw"]').one('click', function() {
   let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
   let depositTokenAddress = $(tradedTokenQuery).attr("address");
-  if(depositTokenAddress === "0x0000000000000000000000000000000000000000") {
-    sendTransaction(walletFunctions.depositETH());
-  } 
-  else { 
-    sendTransaction(walletFunctions.withdrawToken(depositTokenAddress));
-  }   
+  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? sendTransaction(walletFunctions.withdrawETH()) : sendTransaction(walletFunctions.withdrawToken(depositTokenAddress));   
 });
