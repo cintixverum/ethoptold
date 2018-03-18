2e07a92b5da3b9bb4f5d0b36388685bbf9f4e4b9
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 12:26:15 +0000

    Update sendTransaction.js

--- js/sendTransaction.js
+++ js/sendTransaction.js
@@ -14,12 +14,10 @@ function sendTransactionCallback(Tx,callback) {
     filter.watch(function(error, result) {
       if (!error) {
         web3.eth.getBlockNumber(function(err,blockNumber) {
-          console.log(blockNumber);
           web3.eth.getBlock(parseInt(blockNumber) - 11, function(err,confirmedBlock) {
             if (confirmedBlock.transactions.length > 0) {
               confirmedBlock.transactions.forEach(function(txId) {
                 web3.eth.getTransaction(txId,function(err,transaction) {
-                  console.log(transaction);
                   if (transaction.to == "0x8f225bffa2fca0852f727787227c4ce97adb572e") {
                     if(callback) {
                       callback();
