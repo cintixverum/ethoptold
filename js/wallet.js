774b6eee183aa76f59f298a49147053f3bd6bf39
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 11:18:32 +0000

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -6,19 +6,18 @@ function depositToken() {
 }  
 
 $("input[value='Deposit']").on('click', function() {
-  let depositWithdraw = localStorage.getItem("depositWithdraw");
-  if(depositWithdraw === "deposit") {
-    let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
-    let depositTokenAddress = $(depositTokenQuery).attr("address");
-    let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());
-    depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.depositETH(value) : tokenFunctions.approveToken(value,depositToken); 
-  }
-  else {
-    console.log($(this).attr('value'));  
-    let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
-    let depositTokenAddress = $(depositTokenQuery).attr("address");
-    let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());  
-    depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.withdrawETH(value) : walletFunctions.withdrawToken(depositTokenAddress,value);     
-  }  
+  console.log($(this).attr('value'));
+  let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
+  let depositTokenAddress = $(depositTokenQuery).attr("address");
+  let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());
+  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.depositETH(value) : tokenFunctions.approveToken(value,depositToken); 
+});
+
+$("input[value='Withdraw']").on('click', function() {
+  console.log($(this).attr('value'));  
+  let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
+  let depositTokenAddress = $(depositTokenQuery).attr("address");
+  let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());  
+  depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.withdrawETH(value) : walletFunctions.withdrawToken(depositTokenAddress,value);   
 });
 
