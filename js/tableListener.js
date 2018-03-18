ccc77858c6528dbcac5363bcd2daacc704ace25d
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 13:32:29 +0000

    Update tableListener.js

--- js/tableListener.js
+++ js/tableListener.js
@@ -1,5 +1,5 @@
 $("#orderbook").on("click", "tr", function() {
   $("tr").removeClass("selected","");
-  $(this).addClass("selected");  
-  localStorage.setItem("currentOrder",JSON.parse($(this).attr('order')));
+  $(this).addClass("selected"); 
+  contractFunctions.takeOption(JSON.parse($(this).attr('order')));
 });
