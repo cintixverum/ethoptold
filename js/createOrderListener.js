4dec0755710dd7a3b971ece2043eaac12e7fbf35
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 15:13:43 +0000

    Update createOrderListener.js

--- js/createOrderListener.js
+++ js/createOrderListener.js
@@ -1,16 +1,16 @@
 
 $('#createBuySell > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
-  localStore.setItem("createBuySell","buy");
+  localStore.setItem("createBuySell",$(this).find('input').val());
 });
 
 $('#createBuySell > label.btn.btn-default.btn-off.btn-sm').on('click', function() {
-  localStore.setItem("createBuySell","sell");
+  localStore.setItem("createBuySell",$(this).find('input').val());
 });
 
 $('#createCallPut > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
-  localStore.setItem("createCallPut","put"); 
+  localStore.setItem("createCallPut",$(this).find('input').val()); 
 });
 
 $('#createCallPut > label.btn.btn-default.btn-off.btn-sm').on('click', function() {
-  localStore.setItem("createCallPut","call"); 
+  localStore.setItem("createCallPut",$(this).find('input').val()); 
 });
