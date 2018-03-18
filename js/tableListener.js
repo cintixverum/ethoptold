9508bb88c26e568d019d7657694794c6f19a8d04
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 13:05:12 +0000

    Update tableListener.js

--- js/tableListener.js
+++ js/tableListener.js
@@ -1,4 +1,5 @@
-$("#orderbook").one("click", "tr", function() {
-  $(this).addClass('selected');  
+$("#orderbook").on("click", "tr", function() {
+  $("tr").removeClass("selected","");
+  $(this).addClass("selected");  
   localStorage.setItem("currentOrder",JSON.parse($(this).attr('order')));
 });
