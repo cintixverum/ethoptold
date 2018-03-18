9c16db52d8c7d8e6c3ba46c113b717da6f508a08
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 15:28:45 +0000

    Create placeOrder.js

---
+++ js/placeOrder.js
@@ -0,0 +1,6 @@
+$('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > form > input').on('click', function() {
+  let orderTimeframe = localStore.getItem("orderTimeframe");
+  let strike_price = $('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > form > div:nth-child(2) > div > input').val();  
+  let amount = $('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > form > div:nth-child(3) > div > input').val();
+  console.log(strike_price,amount,orderTimeframe);
+});  
