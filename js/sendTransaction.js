9acf6bfb94919d8ff2ebb174528d354d49e14337
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 12:16:47 +0000

    Update sendTransaction.js

--- js/sendTransaction.js
+++ js/sendTransaction.js
@@ -1,8 +1,6 @@
 function sendTransaction(Tx,callback) {
   web3.eth.sendTransaction(Tx, function(err,transactionHash) {
     if (err || !transactionHash) return;
-    console.log("Transaction Hash: ", transactionHash," Err: ",err);
-    console.log(callback);
     if(callback) {
       callback();
     }  
@@ -12,22 +10,23 @@ function sendTransaction(Tx,callback) {
 function sendTransactionCallback(Tx,callback) {
   web3.eth.sendTransaction(Tx, function(err,transactionHash) {
     if (err || !transactionHash) return;
-    console.log("sendTransactionCallback");
     let filter = web3.eth.filter('latest')
     filter.watch(function(error, result) {
       if (!error) {
-        let confirmedBlock = web3.eth.getBlock(web3.eth.blockNumber - 11)
-        if (confirmedBlock.transactions.length > 0) {
-          confirmedBlock.transactions.forEach(function(txId) {
-            let transaction = web3.eth.getTransaction(txId);
-            console.log(transaction);
-            if (transaction.to == "0x8f225bffa2fca0852f727787227c4ce97adb572e") {
-              if(callback) {
-                callback();
-              }      
-            }
-          })
-        }
+        web3.eth.getBlock(web3.eth.blockNumber - 11, function(err,confirmedBlock) {
+          if (confirmedBlock.transactions.length > 0) {
+            confirmedBlock.transactions.forEach(function(txId) {
+              web3.eth.getTransaction(txId,function(err,transaction) {
+                console.log(transaction);
+                if (transaction.to == "0x8f225bffa2fca0852f727787227c4ce97adb572e") {
+                  if(callback) {
+                    callback();
+                  }      
+                }
+              });  
+            });
+          }
+        });    
       }
     })       
   });
