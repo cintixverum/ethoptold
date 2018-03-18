6cc19fb61de37e72113f713fb222d41400f8c3b2
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 11:09:17 +0000

    Update depositWithdrawButton.js

--- js/depositWithdrawButton.js
+++ js/depositWithdrawButton.js
@@ -1,11 +1,9 @@
 $('#status > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
   $('#body > div.wallet-container > div > div > form > input').val('Deposit');
   localStorage.setItem('depositWithdraw','deposit');
-  console.log(localStorage.getItem("depositWithdraw"));
 });
 
 $('#status > label.btn.btn-default.btn-off.btn-sm').on('click', function() {
   $('#body > div.wallet-container > div > div > form > input').val('Withdraw');
   localStorage.setItem('depositWithdraw','withdraw');  
-  console.log(localStorage.getItem("depositWithdraw"));
 });
