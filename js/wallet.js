c74532a414027ed9c6527df812bfc6c694f24c42
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 13:12:32 +0000

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -2,7 +2,7 @@ function depositToken() {
   let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
   let depositTokenAddress = $(depositTokenQuery).attr("address");
   let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());
-  walletFunctions.depositToken(depositTokenAddress,value);
+  contractFunctions.depositToken(depositTokenAddress,value);
 }  
 
 $("#body > div.wallet-container > div > div > form > input").on('click', function() {
@@ -12,7 +12,7 @@ $("#body > div.wallet-container > div > div > form > input").on('click', functio
     let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
     let depositTokenAddress = $(depositTokenQuery).attr("address");
     let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());
-    depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.depositETH(value) : tokenFunctions.approveToken(contractAddress,value,depositToken); 
+    depositTokenAddress === "0x0000000000000000000000000000000000000000" ? contractFunctions.depositETH(value) : tokenFunctions.approveToken(contractAddress,value,depositToken); 
   } 
 });
 
@@ -22,7 +22,7 @@ $("#body > div.wallet-container > div > div > form > input").on('click', functio
     let depositTokenQuery = "p[fullname='" + localStore.getItem("depositToken") + "']";
     let depositTokenAddress = $(depositTokenQuery).attr("address");
     let value = parseFloat($('#body > div.wallet-container > div > div > form > div:nth-child(2) > div > input').val());  
-    depositTokenAddress === "0x0000000000000000000000000000000000000000" ? walletFunctions.withdrawETH(value) : walletFunctions.withdrawToken(depositTokenAddress,value);  
+    depositTokenAddress === "0x0000000000000000000000000000000000000000" ? contractFunctions.withdrawETH(value) : contractFunctions.withdrawToken(depositTokenAddress,value);  
   }  
 });
 
