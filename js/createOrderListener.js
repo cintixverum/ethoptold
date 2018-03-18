95cf9721345f5a467856d1b82414135c13fea044
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 15:22:02 +0000

    Update createOrderListener.js

--- js/createOrderListener.js
+++ js/createOrderListener.js
@@ -1,4 +1,3 @@
-
 $('#createBuySell > label.btn.btn-default').on('click', function() {
   localStore.setItem("createBuySell",$(this).find('input').val());
 });
@@ -6,3 +5,7 @@ $('#createBuySell > label.btn.btn-default').on('click', function() {
 $('#createCallPut > label.btn.btn-default').on('click', function() {
   localStore.setItem("createCallPut",$(this).find('input').val()); 
 });
+
+$('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > form > div:nth-child(1) > select').on('change', function() {
+  localStore.setItem("orderTimeframe",$(this).val());
+})
