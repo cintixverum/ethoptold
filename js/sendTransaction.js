eb65692dfba6dfdc9830b56863ff24cf833420f9
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 12:19:40 +0000

    Update sendTransaction.js

--- js/sendTransaction.js
+++ js/sendTransaction.js
@@ -13,19 +13,21 @@ function sendTransactionCallback(Tx,callback) {
     let filter = web3.eth.filter('latest')
     filter.watch(function(error, result) {
       if (!error) {
-        web3.eth.getBlock(web3.eth.blockNumber - 11, function(err,confirmedBlock) {
-          if (confirmedBlock.transactions.length > 0) {
-            confirmedBlock.transactions.forEach(function(txId) {
-              web3.eth.getTransaction(txId,function(err,transaction) {
-                console.log(transaction);
-                if (transaction.to == "0x8f225bffa2fca0852f727787227c4ce97adb572e") {
-                  if(callback) {
-                    callback();
-                  }      
-                }
-              });  
-            });
-          }
+        web3.eth.blockNumber(function(err,blockNumber) {
+          web3.eth.getBlock(blockNumber - 11, function(err,confirmedBlock) {
+            if (confirmedBlock.transactions.length > 0) {
+              confirmedBlock.transactions.forEach(function(txId) {
+                web3.eth.getTransaction(txId,function(err,transaction) {
+                  console.log(transaction);
+                  if (transaction.to == "0x8f225bffa2fca0852f727787227c4ce97adb572e") {
+                    if(callback) {
+                      callback();
+                    }      
+                  }
+                });  
+              });
+            }
+          });   
         });    
       }
     })       
