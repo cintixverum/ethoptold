c31d0606a5f87f60539f4c87f043f1e654cf5c66
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-18 16:06:42 +0000

    Update placeOrder.js

--- js/placeOrder.js
+++ js/placeOrder.js
@@ -2,5 +2,6 @@ $('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-
   let orderTimeframe = localStore.getItem("orderTimeframe");
   let strike_price = $('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > form > div:nth-child(2) > div > input').val();  
   let amount = $('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > form > div:nth-child(3) > div > input').val();
+  console.log($('.place-order input'));
   console.log(strike_price,amount,orderTimeframe);
 });  
