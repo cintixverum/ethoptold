01e0b318dee9ae030d54848328c70e234e63c720
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-19 13:09:39 +0000

    Update tradeTabListener.js

--- js/tradeTabListener.js
+++ js/tradeTabListener.js
@@ -1,15 +1,15 @@
 $('#status > label.btn.btn-default.btn-on.btn-sm').on('click', function() {
   $('#positionsTab').show();
-  $('#ordersTab').show();  
+  $('#ordersList').show();  
   $('#tradeTab').hide();
 });
 $('#status > label.btn.btn-default.btn-on.btn-sm:nth-child(2)').on('click', function() {
   $('#tradeTab').show();
-  $('#ordersTab').show();    
+  $('#ordersList').show();    
   $('#positionsTab').hide();
 });
 $('#status > label.btn.btn-default.btn-on.btn-sm:nth-child(3)').on('click', function() {
   $('#positionsTab').show();
   $('#tradeTab').show();  
-  $('#ordersTab').hide();
+  $('#ordersList').hide();
 });
