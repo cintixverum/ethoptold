09c21257fed5e43758796d9b5d57b3f37ed4e6c2
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-19 16:04:05 +0000

    Update selectOrder.js

--- js/selectOrder.js
+++ js/selectOrder.js
@@ -1,3 +1,11 @@
 $('#orderbook tr').on('click', function() {
   $(this).addClass('selected');
 });  
+
+$('#positionsTab tr').on('click', function() {
+  $(this).addClass('selected2');
+});  
+
+$('#ordersList tr').on('click', function() {
+  $(this).addClass('selected2');
+});  
