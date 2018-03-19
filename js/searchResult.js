86bf7f0958033654924548110d6d3b9462b7da52
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-19 15:46:04 +0000

    Update searchResult.js

--- js/searchResult.js
+++ js/searchResult.js
@@ -11,7 +11,6 @@ $.get("https://www.optionsdexapi.tk/getApprovedTokens",function(tokenArray,statu
     if(window.location.href === "https://optionsdex.github.io/") {
       updateOrderbook();
       getMyOrders(); 
-      getPositions();  
       localStore.getItem("buySell") === "buy" ? $('#buySell > label.btn.btn-default.btn-on.btn-sm').addClass('active') : $('#buySell > label.btn.btn-default.btn-off.btn-sm').addClass('active');
       localStore.getItem("optionType") === "put" ? $('#optionType > label.btn.btn-default.btn-off.btn-sm').addClass('active') : $('#optionType > label.btn.btn-default.btn-on.btn-sm').addClass('active');
     }       
