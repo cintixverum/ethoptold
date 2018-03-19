c0ee4165c524a4b4d2510775557dd5a414b0c6d2
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-19 14:28:19 +0000

    Create getMyOrder.js

---
+++ js/getMyOrder.js
@@ -0,0 +1,30 @@
+function getMyOrders() {
+  let userAddress = localStorage.getItem("userAddress");
+  $.post('https://www.optionsdexapi.tk/getMyOrders', {
+        'taker': userAddress
+    }, function(data) {
+        data.forEach(function(arr) {
+            let tradedTokenName = localStore.getItem("tradedToken");
+            let baseTokenName = localStore.getItem("baseToken");
+            let tradedTokenQuery = "p[fullname='" + tradedTokenName + "']";
+            let baseTokenQuery = "p[fullname='" + baseTokenName + "']";
+            let timeframeQuery = "option[date='" + localStore.getItem("timeframe") + "']";
+            let tradedTokenAddress = $(tradedTokenQuery).attr("address");
+            let baseTokenAddress = $(baseTokenQuery).attr("address"); 
+            let maturation = new Date(parseInt(arr.maturation)*1000).toUTCString();
+            let purchaseTimestamp = new Date(parseInt(arr.purchaseTimestamp)).toUTCString();
+            let newRow;
+            if(arr.makerIsSeller === "true") {  
+                if(baseTokenAddress === arr.tokenB) {
+                    newRow = '<tr order="' + JSON.stringify(arr).replace(/"/g, "'") + '"><th>' + tokenName + '</th><th>Call</th><th>' + maturation + '</th><th>' + purchaseTimestamp + '</th><th>No</th></tr>';
+                    $('#MyTrades > table > tbody').append(newRow);
+                }
+            } else if(arr.makerIsSeller === "false") {
+                if(baseTokenAddress === arr.tokenA) {
+                    newRow = '<tr order="' + JSON.stringify(arr).replace(/"/g, "'") + '"><th>' + tokenName + '</th><th>Put</th><th>' + maturation + '</th><th>' + purchaseTimestamp + '</th><th>No</th></tr>';
+                    $('#MyTrades > table > tbody').append(newRow);
+                }
+            }
+        });
+  });
+}  
