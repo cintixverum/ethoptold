8ba3b8d0d1f6d919eb7d257c1df83a78d936473a
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-19 16:42:30 +0000

    Update searchResult.js

--- js/searchResult.js
+++ js/searchResult.js
@@ -1,8 +1,6 @@
 $.get("https://www.optionsdexapi.tk/getApprovedTokens",function(tokenArray,status) {
     tokenArray.forEach(function(arr) {
-        console.log(arr);
         let paragraph = '<p fullname="' + arr['token_symbol'] + '" decimals="' + arr['decimals'] +  '" address="' + arr['token_address'] + '" >' + arr['token_symbol'] + '</p>'
-        console.log(paragraph);
         $('#search-dropdown').append(paragraph);
     });
     $('.baseToken').text(localStore.getItem("baseToken"));
