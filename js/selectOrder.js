4e33b6cee3b81428929c7f301455d08f4cb1c7c3
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-19 16:23:44 +0000

    Update selectOrder.js

--- js/selectOrder.js
+++ js/selectOrder.js
@@ -2,12 +2,12 @@ $('#orderbook tr').on('click', function() {
   $(this).addClass('selected');
 });  
 
-$('#positionsTab tr').on('click', function() {
+$('#positionsTab > table > tbody > tr').on('click', function() {
   console.log($(this));
   $(this).addClass('selected2');
 });  
 
-$('#ordersList > table > tbody > tr:nth-child(1)').on('click', function() {
+$('#ordersList > table > tbody > tr').on('click', function() {
   console.log($(this));  
   $(this).addClass('selected2');
 });  
