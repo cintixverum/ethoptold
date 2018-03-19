118ac3b546b1ded88f5b3d84a03f11fedcc7de1b
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-19 11:44:56 +0000

    Create tradeTabListener.js

---
+++ js/tradeTabListener.js
@@ -0,0 +1,3 @@
+$('#status > label:nth-child(2)').on('click', function() {
+  $('#body > div.main > div.row.position > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > div.table-responsive.mt-20').show();
+});  
