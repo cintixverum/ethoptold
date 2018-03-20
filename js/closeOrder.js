862e5e362c3dcf475be7fac176482e5daef3f52f
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-20 17:28:14 +0000

    Update closeOrder.js

--- js/closeOrder.js
+++ js/closeOrder.js
@@ -1,3 +1,4 @@
 $('#positionsTab > div > span:nth-child(1) > button').on('click', function() {
+  let order = JSON.parse(localStorage.getItem("tradeableOrder"));
   contractFunctions.closeOption(order);
 });
