5be2ccb712afe5fc0e37e1d119594fea36a5f722
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-20 14:54:44 +0000

    Update loadTrade.js

--- js/loadTrade.js
+++ js/loadTrade.js
@@ -8,23 +8,27 @@ function loadTrade() {
   let baseTokenName = localStore.getItem("tradeableOrderBaseTokenName");
   let tradedTokenName = localStore.getItem("tradeableOrderTradedTokenName");
   
-  $('.tradeableOrderBaseTokenName').text(baseTokenName);
-  $('.tradeableOrderTradedTokenName').text(tradedTokenName);  
-  
-  let option = "<option>" + baseTokenName  + "</option>";  
-  let option2 = option + "<option>" + tradedTokenName  + "</option>";
-  
-  $('#tradeTab > div:nth-child(1) > select').append(option2);
-  
-  let order = JSON.parse(localStore.getItem("tradeableOrder"));
-  
   let tradedTokenQuery = "p[fullname='" + tradedTokenName + "']";
   let baseTokenQuery = "p[fullname='" + baseTokenName + "']";
   let tradedTokenAddress = $(tradedTokenQuery).attr("address");
   let baseTokenAddress = $(baseTokenQuery).attr("address");
   
+  $('.tradeableOrderBaseTokenName').text(baseTokenName);
+  $('.tradeableOrderTradedTokenName').text(tradedTokenName);  
+  
+  let option = '<option address="' + baseTokenAddress + '" name="' + baseTokenName + '">' + baseTokenName  + "</option>";  
+  let option2 = option + '<option address="' + tradedTokenAddress + '" name="' + tradedTokenName + '">' + tradedTokenName  + "</option>";  
+  
+  $('#tradeTab > div:nth-child(1) > select').append(option2);
+  
+  let order = JSON.parse(localStore.getItem("tradeableOrder"));
+  
   $('#tradeTab > div:nth-child(1) > select').on('click', function() {
-    contractFunctions.getOrderBalance(order,tradedTokenAddress,setBalance);
+    let tokenAddress = $(this).attr("address");
+    let tokenName = $(this).attr("name");
+    $('#tradeTab > div:nth-child(2) > div > span > span').text(tokenName);
+    $('#tradeTab > div:nth-child(3) > div > span > span').text(tokenName);
+    contractFunctions.getOrderBalance(order,tokenAddress,setBalance);
   });  
 
 }  
