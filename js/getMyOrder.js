49393f04b31ac5360f6338f41c4abce08a531c8a
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-20 11:53:01 +0000

    Update getMyOrder.js

--- js/getMyOrder.js
+++ js/getMyOrder.js
@@ -12,8 +12,7 @@ function getMyOrders() {
             let maturation = new Date(parseInt(arr.maturation)*1000).toUTCString();
             let purchaseTimestamp = new Date(parseInt(arr.purchaseTimestamp)).toUTCString();
             let callPut = arr.makerIsSeller === "true" ? "Call" : "Put";
-            let isMaker = arr.maker === "userAddress" ? "Yes" : "No";
-            console.log(isMaker);
+            let isMaker = arr.maker === userAddress ? "Yes" : "No";
             let newRow = '<tr order="' + JSON.stringify(arr).replace(/"/g, "'") + '"><th>' + tradedTokenName + '</th><th>' + callPut  + '</th><th>' + purchaseTimestamp + '</th><th>' + maturation + '</th><th>' + isMaker + '</th></tr>';
             $('#ordersTable').append(newRow);
             if(arr.expiration >= currentTime && arr.maturation >= currentTime) {
