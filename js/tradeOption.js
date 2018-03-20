47dfeaf6380cbdb808166271799bd69d7f1aa665
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-20 17:06:56 +0000

    Update tradeOption.js

--- js/tradeOption.js
+++ js/tradeOption.js
@@ -1,4 +1,24 @@
+function toFixedNumber(i) {
+    console.log(i);
+    if(~i.toString().indexOf("e")) {
+        let num = parseInt(i);
+        while(i > 1) {
+            num += "0";
+            i /= 10;
+        }
+        return num;
+    }
+    return i.toString();
+}
+
 $('#tradeTab > input').on('click', function() {
   let order = JSON.parse(localStore.getItem("tradeableOrder"));
-  contractFunctions.tradeOptions();
+  let tokenName = $('#tradeTab > div:nth-child(1) > select').val();
+  let element = $('option[name="' + tokenName + '"]');
+  let tokenAddress = element.attr("address");
+  let amount =  new BigNumber($('#tradeTab > div:nth-child(3) > div > input').val());
+  let isTradingTokenA = tokenAddress === order.tokenA;
+  let decimals = isTradingTokenA ? parseInt(order.tokenADecimals) : parseInt(order.tokenBDecimals); 
+  let adjustedAmount = toFixedNumber(amount.multipliedBy(decimals));
+  contractFunctions.tradeOptions(order,adjustedAmount,isTradingTokenA);
 });
