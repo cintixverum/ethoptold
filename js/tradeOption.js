19fbe986eb516dd70792d8e38a76648a38370884
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-20 16:00:47 +0000

    Update tradeOption.js

--- js/tradeOption.js
+++ js/tradeOption.js
@@ -7,11 +7,12 @@ $('#tradeTab > div:nth-child(3) > div > input').keyup(function() {
   let amountA = order.tokenA === tokenAddress ? parseFloat(order.limitTokenA) : parseFloat(order.limitTokenB);
   let amountB = order.tokenA === tokenAddress ? parseFloat(order.limitTokenB) : parseFloat(order.limitTokenA);
   let price = (amount*(amountB/amountA)).toFixed(2);
-  console.log(price);
   let baseToken = order.makerIsSeller === "true" ? order.tokenA : order.tokenB;
   let baseTokenName = $('option[address="' + baseToken + '"]').attr('name');
   let tradedToken = order.makerIsSeller === "true" ? order.tokenB : order.tokenA;
   let tradedTokenName = $('option[address="' + tradedToken + '"]').attr('name');
   $('#tradeTab > div.total > div > div:nth-child(1) > div.col-md-4 > p').text(baseTokenName);
-  $('#tradeTab > div.total > div > div:nth-child(2) > div.col-md-4 > p').text(tradedTokenName);    
+  $('#tradeTab > div.total > div > div:nth-child(2) > div.col-md-4 > p').text(tradedTokenName);
+  $('#tradeTab > div.total > div > div:nth-child(1) > div.col-md-8 > span').text(amount);  
+  $('#tradeTab > div.total > div > div:nth-child(2) > div.col-md-8 > span').text(price);
 });  
