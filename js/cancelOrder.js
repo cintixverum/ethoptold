3aba33ec0427020d4630167e378ef5e2e79d9c0f
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-20 17:28:28 +0000

    Update cancelOrder.js

--- js/cancelOrder.js
+++ js/cancelOrder.js
@@ -1,3 +1,4 @@
 $('#positionsTab > div > span:nth-child(2) > button').on('click', function() {
+  let order = JSON.parse(localStorage.getItem("tradeableOrder"));  
   contractFunctions.cancelOptionOrder(order);
 });
