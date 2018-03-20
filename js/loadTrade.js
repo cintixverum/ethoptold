566920df91d0dd5006e701d7f7811e593ffbf549
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-20 13:56:45 +0000

    Update loadTrade.js

--- js/loadTrade.js
+++ js/loadTrade.js
@@ -5,7 +5,7 @@ function loadTrade() {
   $('.tradeableOrderBaseTokenName').text(baseTokenName);
   $('.tradeableOrderTradedTokenName').text(tradedTokenName);  
   
-  if($('#tradeTab > div:nth-child(1) > select').children() === 0) {
+  if($('#tradeTab > div:nth-child(1) > select').children() === 1) {
     let option = "<option>" + baseTokenName  + "</option>";  
     let option2 = option + "<option>" + tradedTokenName  + "</option>";
     $('#tradeTab > div:nth-child(1) > select').append(option2);
