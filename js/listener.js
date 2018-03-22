08acea4dbeed6255cc3ccf6869a0bfabf3f095e1
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-22 12:41:40 +0000

    Update listener.js

--- js/listener.js
+++ js/listener.js
@@ -125,5 +125,9 @@ $("#imaginary_container > div:nth-child(1) > input").keyup(function() {
       }        
     });
   });
+$('#body > div.main > div:nth-child(3) > div.col-lg-4.col-md-5.col-sm-12.col-xs-12 > div > form > input').on('click', function() {
+  contractFunctions.create();
+});  
+
 
 
