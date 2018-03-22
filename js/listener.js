7cca542ddc298d6f79dbf9e7a246c0a270258252
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-22 12:39:16 +0000

    Update listener.js

--- js/listener.js
+++ js/listener.js
@@ -80,3 +80,27 @@ $('#status > label.btn.btn-default.btn-on.btn-sm:nth-child(3)').on('click', func
   $('#tradeTab').hide();  
   $('#ordersList').show();
 });
+  $("#imaginary_container > div:nth-child(1) > input").keyup(function() {
+    var $ps = $("#search-dropdown > p");
+    var val = $.trim(this.value).toUpperCase();
+    if (val === "") {
+        $("#search-dropdown").hide(); 
+    }
+    else {
+        $("#search-dropdown").show(); 
+        $ps.hide();
+        $ps.filter(function() {
+            return -1 != $(this).text().toUpperCase().indexOf(val); }).show();
+    } 
+    $ps.one("click", function() {
+      if(localStore.getItem("tradedToken") !== $(this).attr("fullname")) {
+        localStore.setItem("tradedToken",$(this).attr("fullname"));       
+      }
+      $('.baseToken').text(localStore.getItem("baseToken"));
+      $('.tradedToken').text(localStore.getItem("tradedToken"));
+      $("#imaginary_container > div:nth-child(1) > input").val('');      
+      $("#search-dropdown").hide();  
+      window.location.reload(true);  
+    });
+  });
+
