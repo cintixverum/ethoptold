f1a40a6d6e4bb3592bcd438ac42376b5e8627a05
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-22 12:37:26 +0000

    Delete tableListener.js

--- js/tableListener.js
+++
@@ -1,6 +0,0 @@
-$("#orderbook").on("click", "tr", function() {
-  $("tr").removeClass("selected","");
-  $(this).addClass("selected"); 
-  let order = JSON.parse($(this).attr('order'));
-  contractFunctions.takeOption(order);
-});
