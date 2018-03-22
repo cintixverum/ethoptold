a1d72c50f924199e0af02b719a74713977f0e56a
optionsdex <36543925+optionsdex@users.noreply.github.com>
2018-03-22 12:46:09 +0000

    Update listener.js

--- js/listener.js
+++ js/listener.js
@@ -141,6 +141,9 @@ $('#positionsTab > div > span:nth-child(1) > button').on('click', function() {
   let order = JSON.parse(localStorage.getItem("tradeableOrder"));
   contractFunctions.closeOption(order);
 });
-
+$('#positionsTab > div > span:nth-child(2) > button').on('click', function() {
+  let order = JSON.parse(localStorage.getItem("tradeableOrder"));  
+  contractFunctions.cancelOptionOrder(order);
+})
 
 
